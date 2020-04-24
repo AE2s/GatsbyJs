547e8ae7b2e2e9e9b0cef0570782ff087a64bfb7
Boris <anas.essaghir@gmail.com>
2020-04-24 14:46:27 +0200

    Step 6 : use transformmer, for example read a markdownfile

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,51 @@
 import React from "react"
 import Header from "../components/header"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
+import { css } from "@emotion/core"
+import { rhythm } from "../utils/typography"
 
-export default () => (
+export default ({ data }) => (
   <Layout>
     <Header headerText="Hello Gatsby!" />
-    <p>What a world.</p>
-    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
-    <p>
-      What do I like to do? Lots of course but definitely enjoy building
-      websites.
-    </p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
+
+    <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <div key={node.id}>
+        <h3
+          css={css`
+            margin-bottom: ${rhythm(1 / 4)};
+          `}
+        >
+          {node.frontmatter.title}{" "}
+          <span
+            css={css`
+              color: #bbb;
+            `}
+          >
+            — {node.frontmatter.date}
+          </span>
+        </h3>
+        <p>{node.excerpt}</p>
+      </div>
+    ))}
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`

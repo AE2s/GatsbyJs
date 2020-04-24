5366c8b9baf84f8d5817c570feaccf85c92e9a5f
Boris <anas.essaghir@gmail.com>
2020-04-24 14:11:13 +0200

    Part 4 : use graphQL to show metaData title

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,24 @@
 import React from "react"
 import Header from "../components/header"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
-export default () => (
+export default ({ data }) => (
   <Layout>
     <div style={{ color: `teal` }}>
-      <Header headerText="About Gatsby" />
+      <Header headerText={`About ${data.site.siteMetadata.title}`} />
       <Header headerText="It's pretty cool" />
       <p>Such wow. Very React.</p>
     </div>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
